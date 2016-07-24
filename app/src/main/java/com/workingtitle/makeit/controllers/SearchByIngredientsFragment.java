fc4e25225a5899642a90df1bd69c8ff3b112d920
Brandon Yeh <byeh@uwaterloo.ca>
2016-07-24 14:47:30 -0400

    refactored listeners in SearchByIngredients

--- app/src/main/java/com/workingtitle/makeit/controllers/SearchByIngredientsFragment.java
+++ app/src/main/java/com/workingtitle/makeit/controllers/SearchByIngredientsFragment.java
@@ -33,234 +33,238 @@ import java.util.Arrays;
  */
 public class SearchByIngredientsFragment extends Fragment {
 
-    /** Items entered by the user is stored in this ArrayList variable */
-    ArrayList<String> ingredientList;
-    private Query query;
+  /**
+   * Items entered by the user is stored in this ArrayList variable
+   */
+  ArrayList<String> ingredientList;
+  private Query query;
 
-    /** Declaring an ArrayAdapter to set items to ListView */
-    IngredientsAddedAdapter recipeListAdapter;
+  /**
+   * Declaring an ArrayAdapter to set items to ListView
+   */
+  IngredientsAddedAdapter recipeListAdapter;
 
-    // textview used to add ingredients to the list
-    AutoCompleteTextView ingredientTextView;
+  // textview used to add ingredients to the list
+  AutoCompleteTextView ingredientTextView;
 
-    // ingredients List
-    IngredientsLookupTable lookupTable;
+  // ingredients List
+  IngredientsLookupTable lookupTable;
 
-    // Search Options info
-    private int cookTimeDay;
-    private int cookTimeHour;
-    private int cookTimeMinute;
-    private int numPortions;
+  // Search Options info
+  private int cookTimeDay;
+  private int cookTimeHour;
+  private int cookTimeMinute;
+  private int numPortions;
 
+  // Widgets
+  Button addBtn;
+  Button searchBtn;
+  ListView listView;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+
+    super.onCreate(savedInstanceState);
+
+    // Initialize data for activity
+    query = new Query(getResources().getString(R.string.search_by_ingredients));
+    ingredientList = new ArrayList<String>();
+
+    // Initialize data for activity
+    lookupTable = new IngredientsLookupTable();
+    try {
+      String s = new GetLookupTable().execute().get();
+      lookupTable.populate(s);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // Get parameters passed when activity was created
+    Bundle b = getActivity().getIntent().getExtras();
+
+    if (b != null) {
+      ArrayList<String> ingredients;
+      ingredients = b.getStringArrayList("IngredientList");
+      if (ingredients != null) {
+        ingredientList = (ArrayList<String>) ingredients.clone();
+      }
+    }
+
+    cookTimeDay = -1;
+    cookTimeHour = -1;
+    cookTimeMinute = -1;
+    numPortions = -1;
+
+  }
+
+  @Override
+  public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    super.onCreateView(inflater, container, savedInstanceState);
+
+    final View view = inflater.inflate(R.layout.activity_search_by_ingredients, container, false);
+
+    resetView();
+
+    /** Defining the ArrayAdapter to set items to ListView */
+    recipeListAdapter = new IngredientsAddedAdapter(view.getContext(), ingredientList);
+
+    // Create the adapter and set it to the AutoCompleteTextView
+    final ArrayAdapter<String> suggestionsAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, lookupTable.getLookupTable());
+
+    //Auto-complete Suggestions for Ingredients TextView
+    ingredientTextView = (AutoCompleteTextView) view.findViewById(R.id.txtItem);
+    ingredientTextView.setOnItemClickListener(new AddIngredientClickListener());
+    ingredientTextView.setOnKeyListener(new AddIngredientEnterListener());
+    ingredientTextView.setAdapter(suggestionsAdapter);
+
+    //List View to display the entered ingredients
+    listView = (ListView) view.findViewById(R.id.ingredientsList);
+    listView.setAdapter(recipeListAdapter);
+
+    /** Reference to the button of the layout main.xml */
+    addBtn = (Button) view.findViewById(R.id.btnAdd);
+    searchBtn = (Button) view.findViewById(R.id.btnFeedMe);
+
+    /** Setting the event listener for the add button */
+    addBtn.setOnClickListener(new ItemAddedListener());
+    searchBtn.setOnClickListener(new GoSearchListener());
+
+    return view;
+  }
+
+  private void openOptionsPage() {
+    Intent intent = new Intent(getActivity(), SearchOptions.class);
+    intent.putExtra("ctDay", cookTimeDay);
+    intent.putExtra("ctHour", cookTimeHour);
+    intent.putExtra("ctMin", cookTimeMinute);
+    intent.putExtra("portions", numPortions);
+
+    startActivityForResult(intent, 1);
+  }
+
+  private void openResultsPage() {
+
+    String results = "";
+    System.out.println(ingredientList.size());
+
+    query.setTerms((ArrayList<String>) (ingredientList.clone()));
+
+    try {
+      saveQuery(query);
+      addDefaultIngredients(query);
+      results = new SearchByIngredients().execute(query).get();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    RecipeCollection recipes = new RecipeCollection();
+    recipes.populateRecipeCollection(results);
+
+    Intent intent = new Intent(getActivity(), SearchResults.class);
+    intent.putExtra("RECIPE_COLLECTION", recipes);
+
+    Bundle b = new Bundle();
+    b.putInt("toolbarBackMessage", R.string.search_ingredients_tab);
+    intent.putExtras(b);
+    startActivityForResult(intent, 0);
+
+    resetView();
+  }
+
+  private void addIngredientToList() {
+    String ingredientAdded = ingredientTextView.getText().toString();
+
+    if (!ingredientAdded.isEmpty() && Arrays.asList(lookupTable.getLookupTable()).indexOf(ingredientAdded) != -1) {
+      ingredientList.add(0, ingredientAdded);
+      recipeListAdapter.notifyDataSetChanged();
+      ingredientTextView.setText("");
+    } else {
+      ingredientTextView.setError(getResources().getString(R.string.invalidIngredientMsg));
+    }
+  }
+
+  public void saveQuery(Query query) {
+    final GlobalClass globalVariable = (GlobalClass) getActivity().getApplicationContext();
+    globalVariable.addQuery(query);
+  }
+
+  public void resetView() {
+    // Initialize data for activity
+    query = new Query(getResources().getString(R.string.search_by_ingredients));
+    ingredientList = query.getTerms();
+  }
+
+  public void addDefaultIngredients(Query query) {
+    if (prefIsEnabled("pref_ingr_salt")) {
+      query.addTerm("salt");
+    }
+
+    if (prefIsEnabled("pref_ingr_pepper")) {
+      query.addTerm("pepper");
+    }
+
+    if (prefIsEnabled("pref_ingr_oil")) {
+      query.addTerm("oil");
+    }
+  }
+
+  public boolean prefIsEnabled(String key) {
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+    boolean enabled = sharedPref.getBoolean(key, false);
+    return enabled;
+  }
+
+
+  /**
+   * Defining a click event listener for the button "Add"
+   */
+  protected class ItemAddedListener implements View.OnClickListener {
     @Override
-    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        super.onCreateView(inflater, container, savedInstanceState);
-
-        final View view = inflater.inflate(R.layout.activity_search_by_ingredients, container, false);
-
-        resetView();
-
-        // Get parameters passed when activity was created
-        Bundle b = getActivity().getIntent().getExtras();
-
-        if (b != null){
-            ArrayList<String> ingredients = new ArrayList<String>();
-            ingredients = b.getStringArrayList("IngredientList");
-            if (ingredients != null) {
-                ingredientList = (ArrayList<String>)ingredients.clone();
-            }
-        }
-
-        //Auto-complete Suggestions for Ingredients TextView
-        ingredientTextView = (AutoCompleteTextView) view.findViewById(R.id.txtItem);
-
-        // Create the adapter and set it to the AutoCompleteTextView
-        final ArrayAdapter<String> suggestionsAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, lookupTable.getLookupTable());
-        ingredientTextView.setAdapter(suggestionsAdapter);
-
-        //List View to display the entered ingredients
-        ListView listView = (ListView) view.findViewById(R.id.ingredientsList);
-
-        /** Defining the ArrayAdapter to set items to ListView */
-        recipeListAdapter = new IngredientsAddedAdapter(view.getContext(), ingredientList);
-
-        /** Reference to the button of the layout main.xml */
-        Button addBtn = (Button) view.findViewById(R.id.btnAdd);
-        Button searchBtn = (Button) view.findViewById(R.id.btnFeedMe);
-
-        /** Defining a click event listener for the button "Add" */
-        View.OnClickListener itemAddedListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addIngredientToList();
-            }
-        };
-
-        /** Define a listener to accept text input when pressing enter on keyboard => simulate clicking "Add button"*/
-        ingredientTextView.setOnKeyListener(new View.OnKeyListener(){
-            public boolean onKey(View v, int keyCode, KeyEvent event){
-                if (event.getAction() == KeyEvent.ACTION_DOWN){
-                    switch (keyCode){
-                        case KeyEvent.KEYCODE_DPAD_CENTER:
-                        case KeyEvent.KEYCODE_ENTER:
-                            addIngredientToList();
-                            return true;
-                        default:
-                            break;
-                    }
-                }
-                return false;
-            }
-        });
-
-        /** Define a listener to add an item directly when clicked from the suggestion list */
-        ingredientTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int position,
-                                    long arg3) {
-                addIngredientToList();
-            }
-        });
-
-        /** Defining a click event listener for the button "Feed me" */
-        View.OnClickListener goSearchListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!ingredientList.isEmpty()) {
-                    openResultsPage();
-                }
-                else{
-                    Toast msg = Toast.makeText(getContext(), "Please add ingredient(s).", Toast.LENGTH_LONG);
-                    msg.show();
-                }
-            }
-        };
-
-        /** Setting the event listener for the add button */
-        addBtn.setOnClickListener(itemAddedListener);
-        searchBtn.setOnClickListener(goSearchListener);
-
-        /** Setting the adapter to the ListView */
-        listView.setAdapter(recipeListAdapter);
-
-        // Go to Search Options Page
-        //Button optionsBtn = (Button) view.findViewById(R.id.btnOptions);
-        //optionsBtn.setOnClickListener(new View.OnClickListener() {
-          //  @Override
-           // public void onClick(View v) {
-             //   openOptionsPage();
-           // }
-        //});
-
-        return view;
+    public void onClick(android.view.View v) {
+      addIngredientToList();
     }
+  }
 
+  /**
+   * Defining a click event listener for the button "Feed me"
+   */
+  protected class GoSearchListener implements View.OnClickListener {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-
-        // Initialize data for activity
-        query = new Query(getResources().getString(R.string.search_by_ingredients));
-
-        // Initialize data for activity
-        lookupTable  = new IngredientsLookupTable();
-        try{
-            String s  = new GetLookupTable().execute().get();
-            lookupTable.populate(s);
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        cookTimeDay = -1;
-        cookTimeHour = -1;
-        cookTimeMinute = -1;
-        numPortions = -1;
-
+    public void onClick(View v) {
+      if (!ingredientList.isEmpty()) {
+        openResultsPage();
+      } else {
+        Toast msg = Toast.makeText(getContext(), "Please add ingredient(s).", Toast.LENGTH_LONG);
+        msg.show();
+      }
     }
+  }
 
-//    private void openOptionsPage() {
-//        Intent intent = new Intent(getActivity(), SearchOptions.class);
-//        intent.putExtra("ctDay", cookTimeDay);
-//        intent.putExtra("ctHour", cookTimeHour);
-//        intent.putExtra("ctMin", cookTimeMinute);
-//        intent.putExtra("portions", numPortions);
-//
-//        startActivityForResult(intent, 1);
-//    }
-
-    private void openResultsPage(){
-
-        String results = "";
-        System.out.println(ingredientList.size());
-
-        query.setTerms((ArrayList<String>)(ingredientList.clone()));
-
-        try{
-            saveQuery(query);
-            addDefaultIngredients(query);
-            results  = new SearchByIngredients().execute(query).get();
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        RecipeCollection recipes = new RecipeCollection();
-        recipes.populateRecipeCollection(results);
-
-        Intent intent = new Intent(getActivity(), SearchResults.class);
-        intent.putExtra("RECIPE_COLLECTION",recipes);
-
-        Bundle b = new Bundle();
-        b.putInt("toolbarBackMessage", R.string.search_ingredients_tab);
-        intent.putExtras(b);
-        startActivity(intent);
-        //startActivityForResult(intent, 0);
-
-        resetView();
+  /** Define a listener to add an item directly when clicked from the suggestion list */
+  protected class AddIngredientClickListener implements AdapterView.OnItemClickListener {
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int position,
+                            long arg3) {
+      addIngredientToList();
     }
+  }
 
-    private void addIngredientToList(){
-        String ingredientAdded = ingredientTextView.getText().toString();
-
-        if (!ingredientAdded.isEmpty() && Arrays.asList(lookupTable.getLookupTable()).indexOf(ingredientAdded) != -1) {
-            ingredientList.add(0, ingredientAdded);
-            recipeListAdapter.notifyDataSetChanged();
-            ingredientTextView.setText("");
+  /** Define a listener to accept text input when pressing enter on keyboard => simulate clicking "Add button"*/
+  protected class AddIngredientEnterListener implements View.OnKeyListener {
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+      if (event.getAction() == KeyEvent.ACTION_DOWN) {
+        switch (keyCode) {
+          case KeyEvent.KEYCODE_DPAD_CENTER:
+          case KeyEvent.KEYCODE_ENTER:
+            addIngredientToList();
+            return true;
+          default:
+            break;
         }
-        else {
-            ingredientTextView.setError(getResources().getString(R.string.invalidIngredientMsg));
-        }
+      }
+      return false;
     }
+  }
 
-    public void saveQuery(Query query){
-        final GlobalClass globalVariable = (GlobalClass) getActivity().getApplicationContext();
-        globalVariable.addQuery(query);
-    }
-
-    public void resetView(){
-        // Initialize data for activity
-        query = new Query(getResources().getString(R.string.search_by_ingredients));
-        ingredientList = query.getTerms();
-    }
-
-    public void addDefaultIngredients(Query query){
-        if (prefIsEnabled("pref_ingr_salt")){
-            query.addTerm("salt");
-        }
-
-        if (prefIsEnabled("pref_ingr_pepper")){
-            query.addTerm("pepper");
-        }
-
-        if (prefIsEnabled("pref_ingr_oil")){
-            query.addTerm("oil");
-        }
-    }
-
-    public boolean prefIsEnabled(String key){
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        boolean enabled = sharedPref.getBoolean(key, false);
-        return enabled;
-    }
 }
